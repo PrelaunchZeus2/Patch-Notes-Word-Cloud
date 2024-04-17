c742f334598d33146f3dc9559700a086b7702fd2
BuildTools <unconfigured@null.spigotmc.org>
2024-04-16 19:52:33 -0700

    Changed to Reddit API instead of Soup HTMP parsing

--- patchnotewordcloud.py
+++ patchnotewordcloud.py
@@ -1,26 +1,39 @@
 import requests
-from bs4 import BeautifulSoup
+import praw
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import datetime
+import os
+
+#Reddit Api Cridentials
+CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
+USER_AGENT = os.getenv('REDDIT_USER_AGENT')
 
 def main():
-
+    testurl = 'https://www.reddit.com/r/leagueoflegends/comments/1c5mymo/patch_148_notes/'
     url = input("Enter the URL of Reddit Post:")
-    create_word_cloud(url)
+    redditpage = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
+    submission = redditpage.submission(url=url)
 
+    comments = []
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        comments.append(comment.body)
     
-def create_word_cloud(reddit_url):
-    response = requests.get(reddit_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = ' '.join(map(lambda p: p.text, soup.find_all('p')))
-    wordcloud = WordCloud(max_words=1000).generate(text)  # Increase max_words to 1000
+    text = ' '.join(comments)
+    wordclout = WordCloud(max_words=1000).generate(text)
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
     plt.show()
-    current_date = datetime.datetime.now()
+    current_date = datetime.tatetime.now()
     date_string = current_date.strftime("%Y-%m-%d")
     plt.savefig(f'wordcloud_{date_string}.png')
+
+    
+
+    
+
 
 if __name__ == '__main__':
     main()
